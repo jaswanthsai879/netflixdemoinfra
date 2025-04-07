5bee194bd6fcb442e41442af4c54398886169732
jaswanthsai879 <87112687+jaswanthsai879@users.noreply.github.com>
2025-04-07 19:34:46 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,13 @@
-provider "aws"{
+provider "aws" {
 region = "us-east-1"
 }
 
 resource "aws_instance" "one" {
 count = 4
-ami = "ami-00a929b66ed6e0de6"
+ami = "ami-04aa00acb1165b32a"
 instance_type = "t2.medium"
+key_name = "argocd"
+vpc_security_group_ids = ["sg-06785bc60f40dceda"]
 tags = {
 Name = var.instance_names[count.index]
 }
